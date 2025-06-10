398454c6e3986be010092c3bf5c8302b061f6a47
yashvishah-20 <yashvishah2036@gmail.com>
2025-06-10 12:48:37 +0530

    Create data_cleanup_example.py

---
+++ data_cleanup_example.py
@@ -0,0 +1,15 @@
+# here, we are given the data of jobs in which there are two mistakes job_date is of string data type so we have to convert it to date and job_skills is of string type so we will convert it in list
+from datetime import datetime
+import ast
+data_science_jobs = [
+    {'job_title': 'Data Scientist', 'job_skills': "['Python', 'SQL', 'Machine Learning']", 'job_date': '2023-05-12'},
+    {'job_title': 'Machine Learning Engineer', 'job_skills': "['Python', 'TensorFlow', 'Deep Learning']", 'job_date': '2023-05-15'},
+    {'job_title': 'Data Analyst', 'job_skills': "['SQL', 'R', 'Tableau']", 'job_date': '2023-05-10'},
+    {'job_title': 'Business Intelligence Developer', 'job_skills': "['SQL', 'PowerBI', 'Data Warehousing']", 'job_date': '2023-05-08'},
+    {'job_title': 'Data Engineer', 'job_skills': "['Python', 'Spark', 'Hadoop']", 'job_date': '2023-05-18'},
+    {'job_title': 'AI Specialist', 'job_skills': "['Python', 'PyTorch', 'AI Ethics']", 'job_date': '2023-05-20'}
+]
+for job in data_science_jobs:
+    job['job_date']=datetime.strptime(job['job_date'],'%Y-%m-%d')
+    job['job_skills']=ast.literal_eval(job['job_skills'])
+print(data_science_jobs)
