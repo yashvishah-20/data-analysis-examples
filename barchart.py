af9ad87020aa81c78405fe91457fea91a7643796
yashvishah-20 <yashvishah2036@gmail.com>
2025-06-11 19:40:10 +0530

    this is a bar graph of sample data set

---
+++ barchart.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv(r"C:\Users\Yashvi\Downloads\data_jobs.csv")
+job_counts=df.job_title_short.value_counts()
+plt.barh(job_counts.index,job_counts)
+plt.show()
