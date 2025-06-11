35a0cc9595d842c5f02ec5018b77d0ccfda33166
yashvishah-20 <yashvishah2036@gmail.com>
2025-06-11 13:02:26 +0530

    Create cleaning_data.py

---
+++ cleaning_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Yashvi\Downloads\data_jobs.csv")
+
+df['job_posted_date']=pd.to_datetime(df.job_posted_date)#converting string to datetime
+df['job_posted_month']=df.job_posted_date.dt.month#making a new column for month
+df.sort_values(by='job_posted_date', ascending=False, inplace=True)#sorting the date on the basisi of job_post_date
+df.drop(labels='salary_hour_avg',axis=1,inplace=True)#deleting the column salary_hour_average because it has no data
+df.dropna(subset=['salary_year_avg'],inplace=True)#deleting all the rows that has Na value in salary_year_avg
+print(df)
+print(df.info())
